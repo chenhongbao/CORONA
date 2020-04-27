b3a95a4a58fc22da6adcab86513543dae4ee9d28
陈宏葆 <chenhongbao@outlook.com>
2020-04-27 10:02:05 +0800

    Add comm/marg/inst support.

--- system.api/src/com/nabiki/corona/kernel/biz/api/TradeLocal.java
+++ system.api/src/com/nabiki/corona/kernel/biz/api/TradeLocal.java
@@ -1,6 +1,9 @@
 package com.nabiki.corona.kernel.biz.api;
 
 import com.nabiki.corona.kernel.api.KerAccount;
+import com.nabiki.corona.kernel.api.KerCommission;
+import com.nabiki.corona.kernel.api.KerInstrument;
+import com.nabiki.corona.kernel.api.KerMargin;
 import com.nabiki.corona.kernel.api.KerOrderStatus;
 import com.nabiki.corona.kernel.api.KerPositionDetail;
 import com.nabiki.corona.kernel.api.KerTradeReport;
@@ -15,4 +18,10 @@ public interface TradeLocal {
 	void positionDetail(KerPositionDetail p);
 
 	void account(KerAccount a);
+	
+	void instrument(KerInstrument i);
+	
+	void margin(KerMargin m);
+	
+	void commission(KerCommission c);
 }
--- system.api/src/com/nabiki/corona/kernel/biz/api/TradeRemote.java
+++ system.api/src/com/nabiki/corona/kernel/biz/api/TradeRemote.java
@@ -6,4 +6,14 @@ public interface TradeRemote {
 	String name();
 
 	void newOrder(NewOrder o);
+	
+	void instrument(String symbol);
+	
+	void margin(String symbol);
+	
+	void commission(String symbol);
+	
+	void account();
+	
+	void positionDetails();
 }
